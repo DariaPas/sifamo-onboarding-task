3948be768cfa5650e682c03e6fd2471054c79f6b
Daria <dpaslavska001@ikasle.ehu.eus>
2026-05-14 20:00:01 +0200

    order-service is added and OpenAPI specifications are created

--- order-service/src/main/java/com/sifamo/order/infrastructure/adapter/in/web/WelcomeController.java
+++ order-service/src/main/java/com/sifamo/order/infrastructure/adapter/in/web/WelcomeController.java
@@ -1,14 +1,14 @@
-package com.sifamo.customer.infrastructure.adapter.in.web;
+package com.sifamo.order.infrastructure.adapter.in.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome to Spring Boot from Customer";
+		return "Welcome to Spring Boot from Order";
 	}
+
 
 }
