9237a423e0ecf623bcdabad90591f044db004ceb
Daria <dpaslavska001@ikasle.ehu.eus>
2026-05-17 09:51:44 +0200

    Configure customer service database connection

--- customer-service/src/main/java/com/sifamo/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/sifamo/customer/CustomerServiceApplication.java
@@ -3,9 +3,7 @@ package com.sifamo.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // to take away, starts without DB
-
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })   // to take away
+@SpringBootApplication
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
